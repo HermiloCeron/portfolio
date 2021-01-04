9805ec2f2e3492aa66330d0c3a81f4b7a97db8da
ceronhermilo <ceronhermilo@yahoo.com>
2021-01-03 19:31:15 -0600

    Software skills section updated

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// ********************************************* //
+// GRID RANDOM BINARY IMAGE FOR THE MAIN SECTION //
+// ********************************************* //
+
 // Used the example here to create the grid
 // https://stackoverflow.com/questions/57550082/creating-a-16x16-grid-using-javascript
 // For rounding 
@@ -16,4 +20,58 @@ for(let i=1;i<=rows*cols;i++){
     gridElement.id='gridEle'+i;
     gridElement.className="gridElement";
     gridContainer.appendChild(gridElement);
+}
+
+// ************************************** //
+// BUTTONS ON THE SOFTWARE SKILLS SECTION //
+// ************************************** //
+
+// Create an image reference
+//https://www.geeksforgeeks.org/how-to-create-an-image-element-dynamically-using-javascript/
+function frontEnd(){
+    document.getElementById("but1").style.opacity=0.5;
+    document.getElementById("but2").style.opacity=1;
+    document.getElementById("but3").style.opacity=1;
+    document.getElementById("but4").style.opacity=1;
+    document.getElementById("text").innerText="A summary of my experience at front end web development";
+    document.getElementById("img").src="./pics/front-end-web.jpg";
+    document.getElementById("img").alt="My front end web development projects";
+}
+
+frontEnd();
+
+// Onclick reference:
+// https://www.w3schools.com/jsref/event_onclick.asp
+document.getElementById("but1").onclick=function(){
+    frontEnd();
+}
+
+document.getElementById("but2").onclick=function(){
+    document.getElementById("but1").style.opacity=1;
+    document.getElementById("but2").style.opacity=0.5;
+    document.getElementById("but3").style.opacity=1;
+    document.getElementById("but4").style.opacity=1;
+    document.getElementById("text").innerText="A summary of my experience at full stack development";
+    document.getElementById("img").src="./pics/full-stack.jpg";
+    document.getElementById("img").alt="My full stack development projects";
+}
+
+document.getElementById("but3").onclick=function(){
+    document.getElementById("but1").style.opacity=1;
+    document.getElementById("but2").style.opacity=1;
+    document.getElementById("but3").style.opacity=0.5;
+    document.getElementById("but4").style.opacity=1;
+    document.getElementById("text").innerText="A summary of my experience at front end framework";
+    document.getElementById("img").src="./pics/front-end-framework.jpg";
+    document.getElementById("img").alt="My front end framework projects";
+}
+
+document.getElementById("but4").onclick=function(){
+    document.getElementById("but1").style.opacity=1;
+    document.getElementById("but2").style.opacity=1;
+    document.getElementById("but3").style.opacity=1;
+    document.getElementById("but4").style.opacity=0.5;
+    document.getElementById("text").innerText="A summary of my experience at native python";
+    document.getElementById("img").src="./pics/native-python.jpg";
+    document.getElementById("img").alt="My native python projects";
 }
